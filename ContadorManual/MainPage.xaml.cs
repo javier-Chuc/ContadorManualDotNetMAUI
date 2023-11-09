609659a7a8f880619febb631d524497395dbc323
Javier Izair Chuc Chi <javierchuc094@gmail.com>
2023-11-09 17:09:55 -0600

    Contador Manual con el conteo configurable

---
+++ ContadorManual/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ContadorManual;
+
+public partial class MainPage : ContentPage
+{
+	private int _conteo;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		_conteo = 0;
+        ConteoLabel.Text = _conteo.ToString();
+	}
+
+    private void OnContarButtonclicked(object sender, EventArgs e)
+    {
+        int rango = (int) RangoDelConteoSlider.Value;
+        _conteo+= rango;
+        ConteoLabel.Text = _conteo.ToString();
+    }
+
+    private void OnReiniciarButtonClicked(object sender, EventArgs e)
+    {
+        _conteo = 0;
+        ConteoLabel.Text = _conteo.ToString();
+    }
+}
+
